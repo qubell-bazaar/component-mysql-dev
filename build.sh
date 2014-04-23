36413b305136ee81b8f16bd06e8d20ebf38c9258
Anton Butovich <abutovich@gmail.com>
2014-04-23 17:35:09 +0300

    CI: Success build [test] [ci skip]

--- build.sh
+++ build.sh
@@ -15,10 +15,23 @@ function check {
     return $status
 }
 
+function berks_install {
+    local INSTALL_PATH=$1
+    
+    berks -d
+    berks install -p ${INSTALL_PATH}
+}
+
 function package {
     local REVISION=$1
+    
+    berks_install berks/cookbooks
+    cd berks
 
     tar -czf ${REPO_NAME}-cookbooks-${REVISION}.tar.gz cookbooks
+    
+    cd ../
+    mv berks/${REPO_NAME}-cookbooks-${REVISION}.tar.gz ${REPO_NAME}-cookbooks-${REVISION}.tar.gz
 }
 
 function publish {
@@ -48,7 +61,7 @@ function publish_github {
     rm -rf *.tar.gz
     git commit -a -m "CI: Success build ${TRAVIS_BUILD_NUMBER} [ci skip]"
     git checkout -b build
-    git push -q origin build:master
+    git push -q origin build:${TRAVIS_BRANCH}
 }
 
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]]; then
