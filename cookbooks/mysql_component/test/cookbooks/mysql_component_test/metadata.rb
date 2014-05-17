9035fd5359c6632c9f174ee29a64291767f75c96
Anton Butovich <abutovich@gmail.com>
2014-05-17 15:52:10 +0300

    Changed component layout

--- cookbooks/mysql_component/test/cookbooks/mysql_component_test/metadata.rb
+++
@@ -1,5 +0,0 @@
-name "mysql_component_test"
-version "0.2.0"
-
-depends "mysql_component", "0.2.0"
-depends "minitest-handler"
--- cookbooks/mysql_component/test/cookbooks/mysql_component_test/recipes/user_create.rb
+++
@@ -1,3 +0,0 @@
-include_recipe "mysql_component"
-include_recipe "mysql_component::user_manage"
-include_recipe "minitest-handler"
